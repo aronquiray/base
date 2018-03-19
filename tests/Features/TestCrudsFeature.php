3e5c23fb35dd1e06b60d084a6299076d04b21aab
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2018-03-19 10:50:36 +0800

    add mockery with test on event firing.
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- tests/Features/TestCrudsFeature.php
+++ tests/Features/TestCrudsFeature.php
@@ -5,10 +5,39 @@ namespace HalcyonLaravel\Base\Tests\Features;
 use HalcyonLaravel\Base\Tests\TestCase;
 use App\Models\Core\Page;
 
+
+
+use HalcyonLaravel\Base\Events\BaseStoringEvent;
+use HalcyonLaravel\Base\Events\BaseStoredEvent;
+
+use HalcyonLaravel\Base\Events\BaseUpdatingEvent;
+use HalcyonLaravel\Base\Events\BaseUpdatedEvent;
+
+use HalcyonLaravel\Base\Events\BaseDeletingEvent;
+use HalcyonLaravel\Base\Events\BaseDeletedEvent;
+
+use HalcyonLaravel\Base\Events\BaseRestoringEvent;
+use HalcyonLaravel\Base\Events\BaseRestoredEvent;
+
+use HalcyonLaravel\Base\Events\BasePurgingEvent;
+use HalcyonLaravel\Base\Events\BasePurgedEvent;
+
+// use Mockery as m;
+// use Event;
+
 class TestCrudsFeature extends TestCase
 {
     public function testLogStore()
     {
+        $this->expectsEvents(BaseStoringEvent::class);
+        $this->expectsEvents(BaseStoredEvent::class);
+
+        // Event::shouldReceive('fire')->with(m::on(function($event){
+        //     return false;
+        // }));
+
+        // Event::shouldIgnoreMissing();
+
         $response = $this->withHeaders([
             'X-Header' => 'Value',
         ])->json('POST', route('admin.page.store'), [
@@ -37,6 +66,10 @@ class TestCrudsFeature extends TestCase
             'status' => 'enable',
         ];
 
+
+        $this->expectsEvents(BaseUpdatingEvent::class);
+        $this->expectsEvents(BaseUpdatedEvent::class);
+
         $response = $this->withHeaders([
             'X-Header' => 'Value',
         ])->json('PUT', route('admin.page.update', $this->page), $dataNew);
@@ -44,7 +77,7 @@ class TestCrudsFeature extends TestCase
         $response
             ->assertStatus(302)
             ->assertSessionHas('flash_success', 'new test title has been updated.')
-            ->assertRedirect(route('admin.page.show',  Page::find(1)));
+            ->assertRedirect(route('admin.page.show', Page::find(1)));
 
         $this->assertDatabaseHas((new Page)->getTable(), $dataNew);
     }
