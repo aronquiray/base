0bcb16843ba372a1aa4b409e9e4771cbb4c7e526
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2019-03-12 11:59:31 +0800

    repo
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

---
+++ src/Criterion/Eloquent/ThisEqualThatCriteria.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lloric
+ * Date: 3/12/19
+ * Time: 11:49 AM
+ */
+
+namespace HalcyonLaravel\Base\Criterion\Eloquent;
+
+use Prettus\Repository\Contracts\CriteriaInterface;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+class ThisEqualThatCriteria implements CriteriaInterface
+{
+    /**
+     * @var string
+     */
+    private $field;
+
+    /**
+     * @var null
+     */
+    private $value;
+    /**
+     * @var string
+     */
+    private $comparison;
+
+    /**
+     * ThisEqualThatCriteria constructor.
+     *
+     * @param string $field
+     * @param null   $value
+     * @param string $comparison
+     */
+    public function __construct(string $field, $value = null, string $comparison = '=')
+    {
+        $this->field = $field;
+        $this->value = $value;
+        $this->comparison = $comparison;
+    }
+
+    /**
+     * @param                                                   $model
+     * @param \Prettus\Repository\Contracts\RepositoryInterface $repository
+     *
+     * @return mixed
+     */
+    public function apply($model, RepositoryInterface $repository)
+    {
+        return $model->where($this->field, $this->comparison, $this->value);
+    }
+
+}
