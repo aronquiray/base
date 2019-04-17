85cc2545a78ce6f9797af5148c21c42268ffbe59
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2019-04-17 10:58:02 +0800

    test in observer
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

--- tests/Units/ObserverTest.php
+++ tests/Units/ObserverTest.php
@@ -4,6 +4,7 @@ namespace HalcyonLaravel\Base\Tests\Units;
 
 use HalcyonLaravel\Base\Tests\App\Repositories\Observer;
 use HalcyonLaravel\Base\Tests\Models\Core\Page;
+use HalcyonLaravel\Base\Tests\Models\Core\PageSoftDelete;
 use HalcyonLaravel\Base\Tests\TestCase;
 use InvalidArgumentException;
 
@@ -25,5 +26,110 @@ class ObserverTest extends TestCase
             }
         };
         $observer->test();
+    }
+
+    /**
+     * @test
+     */
+    public function destroy_not_soft_delete_execute()
+    {
+        $page = Page::create([
+            'title' => 'destroy_not_soft_delete',
+            'status' => 'enable',
+        ]);
+        $observer = new class extends Observer
+        {
+            public function test()
+            {
+                $model = Page::where('title', 'destroy_not_soft_delete')->first();
+                parent::checkBeforeDelete('destroy', $model, function () use ($model) {
+                    $model->update([
+                        'title' => 'im update destroy_not_soft_delete',
+                    ]);
+                });
+            }
+        };
+        $observer->test();
+
+        $this->assertEquals('im update destroy_not_soft_delete', $page->fresh()->title);
+    }
+
+    /**
+     * @test
+     */
+    public function purge_not_soft_delete_not_execute()
+    {
+        $page = Page::create([
+            'title' => 'purge_not_soft_delete_not_execute',
+            'status' => 'enable',
+        ]);
+        $observer = new class extends Observer
+        {
+            public function test()
+            {
+                $model = Page::where('title', 'purge_not_soft_delete_not_execute')->first();
+                parent::checkBeforeDelete('purge', $model, function () use ($model) {
+                    $model->update([
+                        'title' => 'im update purge_not_soft_delete_not_execute',
+                    ]);
+                });
+            }
+        };
+        $observer->test();
+
+        $this->assertEquals('purge_not_soft_delete_not_execute', $page->fresh()->title);
+    }
+
+    /**
+     * @test
+     */
+    public function destroy_soft_delete_not_execute()
+    {
+        $page = PageSoftDelete::create([
+            'title' => 'destroy_soft_delete_not_execute',
+            'status' => 'enable',
+        ]);
+        $observer = new class extends Observer
+        {
+            public function test()
+            {
+                $model = PageSoftDelete::where('title', 'destroy_soft_delete_not_execute')->first();
+                parent::checkBeforeDelete('destroy', $model, function () use ($model) {
+                    $model->update([
+                        'title' => 'im update destroy_soft_delete_not_execute',
+                    ]);
+                });
+            }
+        };
+        $observer->test();
+
+        $this->assertEquals('destroy_soft_delete_not_execute', $page->fresh()->title);
+
+    }
+
+    /**
+     * @test
+     */
+    public function purge_soft_delete_execute()
+    {
+        $page = PageSoftDelete::create([
+            'title' => 'purge_soft_delete_execute',
+            'status' => 'enable',
+        ]);
+        $observer = new class extends Observer
+        {
+            public function test()
+            {
+                $model = PageSoftDelete::where('title', 'purge_soft_delete_execute')->first();
+                parent::checkBeforeDelete('purge', $model, function () use ($model) {
+                    $model->update([
+                        'title' => 'im update purge_soft_delete_execute',
+                    ]);
+                });
+            }
+        };
+        $observer->test();
+
+        $this->assertEquals('im update purge_soft_delete_execute', $page->fresh()->title);
     }
 }
