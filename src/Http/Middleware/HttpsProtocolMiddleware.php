223afc8f06bf5a55e2b2c7d11c87ad6308cc0a82
Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>
2019-05-27 07:48:01 +0800

    Https handler
    Signed-off-by: Lloric Mayuga Garcia <lgarcia.halcyondigital@gmail.com>

---
+++ src/Http/Middleware/HttpsProtocolMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HalcyonLaravel\Base\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\URL;
+
+class HttpsProtocolMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $scheme = 'https';
+
+        if (strpos(app('config')->get('app.url'), "$scheme://") !== false) {
+
+            if (!$request->secure()) {
+                return redirect()->secure($request->getRequestUri());
+            }
+
+            URL::forceScheme($scheme);
+        }
+
+        return $next($request);
+    }
+
+}
